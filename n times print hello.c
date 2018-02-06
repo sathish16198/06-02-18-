6ccbb83ed99248e805438cb3f6a0ca71dfcb0b49
sathish16198 <35728168+sathish16198@users.noreply.github.com>
2018-02-06 16:17:45 +0530

    Create n times print hello.c

---
+++ n times print hello.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int i, n;
+    printf("how may times you will print HELLO:");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++)
+    printf("HELLO\n");
+    return 0;
+    
+}
